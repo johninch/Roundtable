9473a75f9389319362ca495ac61fbaad3548caa9
shujianbo <shujianbo@itiger.com>
2020-07-03 16:55:09 +0800

    搜索功能开发中

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,8 @@ module.exports = {
     head: [
         ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./favicon.ico` }],
+        ['script', { src: './js/search.js' }],
+        ['script', { src: './js/main.js' }],
         // 在移动端, 搜索框在获得焦点时会放大, 并且在失去焦点后可以左右滚动, 这可以通过设置元来优化
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
     ],
